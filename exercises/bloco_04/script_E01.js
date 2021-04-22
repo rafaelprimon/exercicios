a5b74b6fb2673689e2bbc84bb4006af39f3f1637
Rafael_Primon <rafaelfernandesprimon@gmail.com>
2021-04-22 18:48:41 -0300

    Cria arquivo e resolve exercicio 1

---
+++ exercises/bloco_04/script_E01.js
@@ -0,0 +1,17 @@
+let a = 10;
+let b = 15;
+
+//adição
+console.log(a+b);
+
+//subtração
+console.log(a-b);
+
+//multiplicação
+console.log(a/b);
+
+//divisão
+console.log(a/b);
+
+//módulo
+console.log(a%b);
