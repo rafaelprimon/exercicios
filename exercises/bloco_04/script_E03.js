ac0c312dd98e354024f6d9d35a6107a5b97c96e5
Rafael_Primon <rafaelfernandesprimon@gmail.com>
2021-04-22 19:03:43 -0300

    exercicio 3

---
+++ exercises/bloco_04/script_E03.js
@@ -0,0 +1,11 @@
+let a = 10;
+let b = 20;
+let c = 23;
+
+if (a > b && a > c) {
+    console.log("o maior elemento é o a");
+} else if (b > a && b > c) {
+    console.log("o b é o maior");
+} else {
+    console.log("o C é o maior");
+}
