679fb7599227010baa6da9a67689578665bf8bb9
Rafael_Primon <rafaelfernandesprimon@gmail.com>
2021-04-22 18:38:04 -0300

    cria programas de operação aritimica - resolve exercício 1

---
+++ exercises/bloco_04/dia_01/script_E01.js
@@ -0,0 +1,18 @@
+let a = 10;
+let b = 15;
+
+//adição
+console.log(a+b);
+
+//subtração
+console.log(a-b);
+
+//Multiplicação
+console.log(a*b);
+
+//Divisao
+console.log(a/b);
+
+//Módulo
+
+console.log(a%b);
