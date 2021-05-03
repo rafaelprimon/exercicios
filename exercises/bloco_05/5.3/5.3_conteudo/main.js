6fafa62566297367ca3e991ad4fcaf8f828b945d
Rafael_Primon <rafaelfernandesprimon@gmail.com>
2021-05-03 19:47:08 -0300

    resolve exercicios conteudos

--- exercises/bloco_05/5.3/5.3_conteudo/main.js
+++ exercises/bloco_05/5.3/5.3_conteudo/main.js
@@ -4,21 +4,51 @@ const divTres = document.getElementById('divTres');
 const input = document.getElementById('input');
 const myWebpage = document.getElementById('mySpotrybefy');
 
-/*
- Copie esse arquivo e edite apenas ele;
- Crie uma função que adicione a classe 'tech' ao elemento selecionado;
-1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
- Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
-com a classe 'tech';
- Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
-redirecione para alguma página;
-1. Que tal redirecionar para seu portifólio?
- Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
-a cor do mesmo;
 
-Segue abaixo um exemplo do uso de event.target:
-*/
+// 1- Crie uma função que adicione a classe 'tech' ao elemento selecionado;
+// Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
 
+function addClassTech (event) {
+    const techElement = document.querySelector('.tech');
+    techElement.classList.remove('tech');
+    event.target.classList.add('tech');
+    input.value = '';
+}
+
+divUm.addEventListener('click', addClassTech);
+divDois.addEventListener('click', addClassTech);
+divTres.addEventListener('click', addClassTech);
+
+// 2 -Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
+//com a classe 'tech';
+
+input.addEventListener ('input', (event) => {
+    const techElement = document.querySelector('.tech');
+    techElement.innerText = event.target.value;
+});
+
+
+// 3 - Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
+//redirecione para alguma página;
+//1. Que tal redirecionar para seu portifólio?
+
+myWebpage.addEventListener( 'dblclick', () => {
+    window.location.replace('https://waves.com.br/');
+});
+ 
+
+// 4 - Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
+//a cor do mesmo;
+
+myWebpage.addEventListener('mouseover', (event) => {
+    event.target.style.color = 'red';
+  });
+  
+myWebpage.addEventListener('mouseout', (event) => {
+    event.target.style.color = 'unset';
+  });
+
+  
 function resetText(event) {
   // O Event é passado como um parâmetro para a função.
   event.target.innerText = 'Opção reiniciada';
