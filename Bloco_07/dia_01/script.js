96167203bc73778a57bfdb82f836fc09fe8f7e30
Rafael_Primon <rafaelfernandesprimon@gmail.com>
2021-05-22 19:36:36 -0300

    exercicio 2

--- Bloco_07/dia_01/script.js
+++ Bloco_07/dia_01/script.js
@@ -30,3 +30,28 @@ Modifique as concatenações para template literals .*/
   }
 
   testingScope(true);
+
+  /*Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente.
+
+Utilize template literals para que a chamada console.log(oddsAndEvens); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+
+Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort() . Spoiler: É possível realizar uma função que ordene qualquer array de números.*/
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sort = () => {
+    oddsAndEvens[0] = 2;
+    oddsAndEvens[1] = 3;
+    oddsAndEvens[2] = 4;
+    oddsAndEvens[3] = 7;
+    oddsAndEvens[4] = 10;
+    oddsAndEvens[5] = 13;
+
+    return oddsAndEvens;
+}
+
+console.log(oddsAndEvens);
+
+const sortedArray = sort();
+console.log(`Os números ${sortedArray} se encontram ordenados de forma crescente !`);
+
