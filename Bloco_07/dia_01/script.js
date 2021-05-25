33760230a73400e7f69be8527e8c243afbf2bde5
Rafael_Primon <rafaelfernandesprimon@gmail.com>
2021-05-25 18:42:24 -0300

    com

--- Bloco_07/dia_01/script.js
+++ Bloco_07/dia_01/script.js
@@ -55,3 +55,52 @@ console.log(oddsAndEvens);
 const sortedArray = sort();
 console.log(`Os números ${sortedArray} se encontram ordenados de forma crescente !`);
 
+
+/*Parte II
+Abaixo, você verá algumas especificações de algoritmos para desenvolver. É fundamental que você utilize o que aprendeu sobre let , const , arrow functions , template literals e ternary operator .
+
+Crie uma função que receba um número e retorne seu fatorial.
+
+Na matemática, o fatorial de um número não negativo N , com a notação N! , é o produto de todos os inteiros menores ou iguais a N . Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
+
+Bônus (opcional): tente fazer o mesmo exercício de forma recursiva . Spoiler: É possível resolver com uma linha usando ternary operator .
+
+Crie uma função que receba uma frase e retorne qual a maior palavra.
+Exemplo:
+
+      longestWord("Antônio foi no banheiro e não sabemos o que aconteceu") // retorna 'aconteceu'*/
+
+
+const factorial = number => {
+    let result = 1
+    
+    for (let index = 2; index <= number; index += 1) {
+        result *= index
+     }
+    
+    return result
+    }
+    
+console.log(factorial(5))
+
+// Recursiva
+
+const factorial = number => number > 1 ? number * factorial(number - 1) : 1
+console.log(factorial(5))
+
+const longestWord = text => {
+    let wordArray = text.split(' ')
+    let maxLength = 0
+    let result = ''
+
+    for (const word of wordArray) {
+        if (word.length > maxLength) {
+            maxLength = word.length
+            result = word
+        }
+    }
+
+    return result
+}
+
+console.log(longestWord("Antonio foi no banheiro e não sabemos o que aconteceu"))
--- Bloco_07/dia_01/tempCodeRunnerFile.js
+++ Bloco_07/dia_01/tempCodeRunnerFile.js
@@ -1,17 +1,11 @@
-const oddsAndEvens = [13, 3, 4, 10, 7, 2];
-
-const sort = () => {
-    oddsAndEvens[0] = 2;
-    oddsAndEvens[1] = 3;
-    oddsAndEvens[2] = 4;
-    oddsAndEvens[3] = 7;
-    oddsAndEvens[4] = 10;
-    oddsAndEvens[5] = 13;
-
-    return oddsAndEvens;
-}
-
-console.log(oddsAndEvens);
-
-const sortedArray = sort();
-console.log(`Os números ${sortedArray} se encontram ordenados de forma crescente !`);
+const factorial = number => {
+    let result = 1
+    
+    for (let index = 2; index <= number; index += 1) {
+        result *= index
+     }
+    
+    return result
+    }
+    
+console.log(factorial(5))
