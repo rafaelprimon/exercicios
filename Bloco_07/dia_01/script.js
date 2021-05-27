7ebc98e15ddf815b50d97140665b3818fc3be43c
Rafael_Primon <rafaelfernandesprimon@gmail.com>
2021-05-27 07:22:53 -0300

    j

--- Bloco_07/dia_01/script.js
+++ Bloco_07/dia_01/script.js
@@ -104,3 +104,9 @@ const longestWord = text => {
 }
 
 console.log(longestWord("Antonio foi no banheiro e não sabemos o que aconteceu"))
+
+// Longestword com sort em uma linha.
+
+const longestWord = text => text.split(' ').sort((wordA, wordB) => wordB.length - wordA.length)[0]
+
+console.log(longestWord("Antonio foi no banheiro e não sabemos o que aconteceu"))
