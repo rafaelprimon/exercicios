e2b1f0c6dabacb4e693d5173cf567580e0a066e7
Rafael_Primon <rafaelfernandesprimon@gmail.com>
2021-05-27 07:18:53 -0300

    .

---
+++ exercises/Bloco_08/dia_01/tempCodeRunnerFile.js
@@ -0,0 +1,16 @@
+
+const nomeEmail = (nomeCompleto) => {
+    const email = nomeCompleto.toLowerCase().split(' ').join('_');
+    return { nomeCompleto, email: `${email}@trybe.com`};
+}
+
+const newEmployees = (callback) => {
+    const employees = {
+      id1: callback('Pedro Guerra'),
+      id2: callback('Luiza Drumond'),
+      id3: callback('Carla Paiva'), 
+    }
+    return employees;
+  };
+
+  console.log(newEmployees(nomeEmail));
